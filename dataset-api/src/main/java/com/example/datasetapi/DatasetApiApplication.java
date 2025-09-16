b98ee3560b8a96c913ce6ea6efa5949c995b73e4
MinhKhang17 <nguyenmkhang1712@gmail.com>
2025-09-16 19:31:49 +0700

    first push

---
+++ dataset-api/src/main/java/com/example/datasetapi/DatasetApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.datasetapi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasetApiApplication {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ dataset-api/src/test/java/com/example/datasetapi/DatasetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datasetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
