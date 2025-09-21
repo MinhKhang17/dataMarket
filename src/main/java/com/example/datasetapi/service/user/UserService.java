f8ceb90afc6c8457550b428f6c1a93253b7ab63c
MinhKhang17 <nguyenmkhang1712@gmail.com>
2025-09-21 17:31:38 +0700

    hoan thanh login with google

--- src/main/java/com/example/datasetapi/service/user/UserService.java
+++ src/main/java/com/example/datasetapi/service/user/UserService.java
@@ -3,12 +3,15 @@ package com.example.datasetapi.service.user;
 import com.example.datasetapi.dto.request.LoginRequest;
 import com.example.datasetapi.dto.request.RegisterRequest;
 import com.example.datasetapi.dto.response.ApiResponse;
+import com.example.datasetapi.model.User;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface UserService {
     public Object login(LoginRequest loginRequest, HttpServletResponse response);
     public ResponseEntity<ApiResponse> register(RegisterRequest registerRequest);
 
+    User createUserForLoginByGoogleFlow(OAuth2User oAuth2User);
 }
