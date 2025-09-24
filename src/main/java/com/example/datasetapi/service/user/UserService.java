3ea3b421bc171ca945c5644eb5c698bb54a22caa
QT845 <thinh200584@gmail.com>
2025-09-24 22:26:21 +0700

    Implement update password feature

--- src/main/java/com/example/datasetapi/service/user/UserService.java
+++ src/main/java/com/example/datasetapi/service/user/UserService.java
@@ -2,13 +2,14 @@ package com.example.datasetapi.service.user;
 
 import com.example.datasetapi.dto.request.LoginRequest;
 import com.example.datasetapi.dto.request.RegisterRequest;
+import com.example.datasetapi.dto.request.UpdatePasswordRequest;
 import com.example.datasetapi.dto.response.ApiResponse;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface UserService {
-    public Object login(LoginRequest loginRequest, HttpServletResponse response);
+    public ResponseEntity<ApiResponse> login(LoginRequest loginRequest, HttpServletResponse response);
     public ResponseEntity<ApiResponse> register(RegisterRequest registerRequest);
-
+    public  ResponseEntity<ApiResponse> updatePassword(UpdatePasswordRequest updatePasswordRequest);
 }
