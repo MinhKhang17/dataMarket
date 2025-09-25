080b8ed6551382fb667f523188714eab932f1a06
MinhKhang17 <nguyenmkhang1712@gmail.com>
2025-09-25 11:11:36 +0700

    add note

--- src/main/java/com/example/datasetapi/config/system/DataInitializer.java
+++ src/main/java/com/example/datasetapi/config/system/DataInitializer.java
@@ -1,8 +1,11 @@
 package com.example.datasetapi.config.system;
 
 import com.example.datasetapi.model.Role;
+import com.example.datasetapi.model.User;
 import com.example.datasetapi.repository.RoleRepository;
+import com.example.datasetapi.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -11,18 +14,42 @@ import java.util.*;
 public class DataInitializer implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
-
-    public DataInitializer(RoleRepository roleRepository) {
+    private final PasswordEncoder passwordEncoder;
+private final UserRepository userRepository;
+    public DataInitializer(UserRepository userRepository,RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
     }
+
 
     @Override
     public void run(String... args) throws Exception {
 
       roleRepository.save(new Role("ADMIN"));
 
-        roleRepository.save(new Role("USER"));
+        roleRepository.save(new Role("CONSUMER"));
 
         System.out.println("Roles & permissions initialized.");
+        createConsumerRole();
+        createAdminRole();
+    }
+
+    private void createAdminRole() {
+        User user = new User();
+        user.setUsername("admin");
+        user.setEmail("admin@gmail.com");
+        user.setPassword(passwordEncoder.encode("password"));
+        user.setRole(roleRepository.findByName("ADMIN").get());
+        userRepository.save(user);
+    }
+
+    private void  createConsumerRole() {
+        User user = new User();
+        user.setUsername("user");
+        user.setPassword(passwordEncoder.encode("password"));
+        user.setEmail("consumer@gmail.com");
+        user.setRole(roleRepository.findByName("CONSUMER").get());
+        userRepository.save(user);
     }
 }
