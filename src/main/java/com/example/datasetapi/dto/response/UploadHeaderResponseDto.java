55907cd75297fedbec7eaec25fee5d652bcc2d81
MinhKhang17 <nguyenmkhang1712@gmail.com>
2025-10-12 11:07:51 +0700

    bat exception chinh sua dataset mapper

--- src/main/java/com/example/datasetapi/dto/response/UploadHeaderResponseDto.java
+++ src/main/java/com/example/datasetapi/dto/response/UploadHeaderResponseDto.java
@@ -2,7 +2,11 @@ package com.example.datasetapi.dto.response;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class UploadHeaderResponseDto {
   private long datasetInformationId;
+  //set neu nhu loi header
+  private List<ValidationErrorDto> validationErrorDtos;
 }
