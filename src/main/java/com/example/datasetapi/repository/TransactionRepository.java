5bc4a8fe8d1eab356ae8a691602c62c98975b7f0
QT845 <thinh200584@gmail.com>
2025-10-29 13:10:03 +0700

    Transaction history

--- src/main/java/com/example/datasetapi/repository/TransactionRepository.java
+++ src/main/java/com/example/datasetapi/repository/TransactionRepository.java
@@ -2,7 +2,14 @@ package com.example.datasetapi.repository;
 
 import com.example.datasetapi.model.paySystem.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     boolean existsByTxnRef(String txnRef);
+    @Query("SELECT t FROM Transaction t WHERE t.wallet.id = :walletId ORDER BY t.createdAt DESC")
+    List<Transaction> findByWalletOrderByCreatedAtDesc(@Param("walletId") int walletId);
+
 }
--- src/main/java/com/example/datasetapi/service/payment/TransactionService.java
+++ src/main/java/com/example/datasetapi/service/payment/TransactionService.java
@@ -1,12 +1,18 @@
 package com.example.datasetapi.service.payment;
 
+import com.example.datasetapi.dto.response.ApiResponse;
 import com.example.datasetapi.enums.Datasets.BuyType;
 import com.example.datasetapi.enums.TransferType;
 import com.example.datasetapi.model.paySystem.Transaction;
 import com.example.datasetapi.model.paySystem.Wallet;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface TransactionService {
      Transaction createTransaction(TransferType transferType, double amount, long user_id, Wallet wallet, BuyType buyType);
+
+     ResponseEntity<ApiResponse> getTransactions();
 }
